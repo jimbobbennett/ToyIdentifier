906ba899c24886c75d2cace77a1abffa99ba9277
Jim Bennett <jimbobbennett@mac.com>
2017-12-29 09:37:40 +0000

    Update ApiKeys.cs

--- ToyIdentifier/ApiKeys.cs
+++ ToyIdentifier/ApiKeys.cs
@@ -4,7 +4,14 @@ namespace ToyIdentifier
 {
     public static class ApiKeys
     {
-        public static string ApiKey = "<Your API Key>";
+#error You need to set up your API keys.
+        // Start by registering for an account at https://customvision.ai
+        // Then create a new project.
+        // From the settings tab, find:
+        // Prediction Key
+        // Project Id
+        // and update the values below
+        public static string PredictionKey = "<Your Prediction Key>";
         public static Guid ProjectId = Guid.Parse("<Your Project GUID>");
     }
 }
